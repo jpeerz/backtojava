d485e92e38b5d3754ebca14c2833175baa05dd84
Jeff P <jeff@siccr.com>
2018-07-03 08:40:56 -0600

    Include unit test sample

---
+++ web/src/test/java/com/siccr/welcome/WelcomeTest.java
@@ -0,0 +1,14 @@
+package com.siccr.welcome;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class WelcomeTests {
+
+    @Test
+    void ItWorksTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
